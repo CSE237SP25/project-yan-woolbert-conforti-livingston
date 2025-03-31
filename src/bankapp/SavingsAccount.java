259dabaa3ab36ee68604a1c7214817bac34e0a67
Cara Conforti <c.conforti@wustl.edu>
2025-03-31 12:20:09 -0500

    a user can now open a savings account

--- src/bankapp/SavingsAccount.java
+++ src/bankapp/SavingsAccount.java
@@ -1,5 +1,15 @@
 package bankapp;
+import java.time.LocalTime;
 
-public class SavingsAccount {
+
+public class SavingsAccount extends BankAccount {
+	private static double interestRate;
+	private static LocalTime interestTime;
+
+	public SavingsAccount(double interestRate, LocalTime interestTime) {
+		super();
+		SavingsAccount.interestRate = interestRate;
+		SavingsAccount.interestTime = interestTime;
+	}
 
 }
